6520e6b10a7dc885138ab006e71fbbd5df33572e
Matusek Márton <matusek.marton@autsoft.hu>
2021-03-03 09:36:36 +0100

    Init Spring Boot project

---
+++ src/test/kotlin/hu/bme/aut/textanalyzer/TextanalyzerApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.bme.aut.textanalyzer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TextanalyzerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

cba39a74db5aa5c9be15ba6b63e9df20c47cb31b
matusekma <matusekma@gmail.com>
2021-05-05 08:51:09 +0200

    Configure API gateway with test values

--- gateway/src/main/kotlin/hu/bme/aut/gateway/GatewayConfig.kt
+++ gateway/src/main/kotlin/hu/bme/aut/gateway/GatewayConfig.kt
@@ -4,17 +4,39 @@ import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory
+
+import org.springframework.beans.factory.annotation.Autowired
+
 
 @Configuration
-class GatewayConfig {
+class GatewayConfig(
+
+    @Autowired
+    private val filterFactory: TokenRelayGatewayFilterFactory
+) {
 
     @Bean
     fun routeLocator(rb: RouteLocatorBuilder): RouteLocator =
         rb.routes()
-            .route("login-route") { routeSpec ->
-                routeSpec
-                    .path("/login")
-                    .uri("lb://auth")
+            .route("messages") { routeSpec ->
+                routeSpec.path("/messages")
+                    .filters { f ->
+                        f.filters(filterFactory.apply())
+                            .removeRequestHeader("Cookie")
+                    }
+                    .uri("lb://executor:8081")
+            }.route("ping") { routeSpec ->
+                routeSpec.path("/ping/**")
+                    .filters { f ->
+                        f.filters(filterFactory.apply())
+                            .removeRequestHeader("Cookie")
+                    }
+                    .uri("lb://executor:8081")
+            }
+            .route("messages") { routeSpec ->
+                routeSpec.path("/register")
+                    .uri("lb://auth:9000")
             }
             .build()
 }

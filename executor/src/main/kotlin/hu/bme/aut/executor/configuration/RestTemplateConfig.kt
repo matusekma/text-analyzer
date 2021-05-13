171e9216157b8f6fd9c4fb830ffcc612be29d6d1
matusekma <matusekma@gmail.com>
2021-05-13 13:00:08 +0200

    Remove @LoadBalanced

--- executor/src/main/kotlin/hu/bme/aut/executor/configuration/RestTemplateConfig.kt
+++ executor/src/main/kotlin/hu/bme/aut/executor/configuration/RestTemplateConfig.kt
@@ -1,7 +1,6 @@
 package hu.bme.aut.executor.configuration
 
 import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
@@ -10,6 +9,5 @@ import org.springframework.web.client.RestTemplate
 class RestTemplateConfig {
 
     @Bean
-    @LoadBalanced
     fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate = restTemplateBuilder.build()
 }

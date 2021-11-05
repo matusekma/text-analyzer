5686ba974b6309eeb949b84bff25520f37a43571
Matusek Márton <matusekma@gmail.com>
2021-11-05 09:45:25 +0100

    Merge main

--- auth/src/main/kotlin/hu/bme/aut/textanalyzerauth/configuration/DefaultSecurityConfig.kt
+++ auth/src/main/kotlin/hu/bme/aut/textanalyzerauth/configuration/DefaultSecurityConfig.kt
@@ -2,8 +2,10 @@ package hu.bme.aut.textanalyzerauth.configuration
 
 import hu.bme.aut.textanalyzerauth.auth.CustomUserDetailsManager
 import hu.bme.aut.textanalyzerauth.repository.UserRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer.withDefaults
@@ -16,27 +18,38 @@ import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler
 
 @Configuration
 @EnableWebSecurity
 class DefaultSecurityConfig(
-        val userRepository: UserRepository
+    @Value("\${jwt.accessTokenCookieName}")
+    private val accessTokenCookieName: String,
+    private val userRepository: UserRepository
 ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http
-                .cors(withDefaults())
-                .csrf { csrf -> csrf.disable() } //.ignoringAntMatchers("/api/admin/login")
-                .authorizeRequests { authorizeRequests ->
-                    authorizeRequests.antMatchers("/login", "/register").permitAll()
-                            .antMatchers("/api/**").authenticated()
-                }
-                .sessionManagement { sessionManagement ->
-                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                }
+            .cors(withDefaults())
+            .csrf { csrf -> csrf.disable() } //.ignoringAntMatchers("/api/admin/login")
+            .authorizeRequests { authorizeRequests ->
+                authorizeRequests
+//                .antMatchers("/api/cases/*", "/api/cases/*/comments").hasAnyAuthority(Role.ASSISTANT.getValue(), Role.DOCTOR.getValue(), Role.PATIENT.getValue())
+//                .antMatchers("/api/registration-requests/**", "/api/admin/cases").hasAnyAuthority(Role.ASSISTANT.getValue(), Role.DOCTOR.getValue())
+//                .antMatchers("/api/assistants", "/api/cases/*/status").hasAuthority(Role.DOCTOR.getValue())
+//                .antMatchers("/api/children", "/api/children/*/cases", "/api/patient/cases", "/api/patient/cases/*/files").hasAuthority(Role.PATIENT.getValue())
+                    .antMatchers("/**").permitAll()
+            }
+            .sessionManagement { sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .logout {
+                it.permitAll()
+                it.logoutSuccessHandler(HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+                it.logoutUrl("/logout")
+                it.deleteCookies(accessTokenCookieName)
+                it.invalidateHttpSession(false)
+            }
     }
 
     @Bean
@@ -46,31 +59,36 @@ class DefaultSecurityConfig(
 
     @Bean
     override fun userDetailsService(): UserDetailsService =
-            CustomUserDetailsManager(userRepository)
+        CustomUserDetailsManager(userRepository)
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
-    fun authProvider(passwordEncoder: PasswordEncoder, userDetailsService: UserDetailsService): DaoAuthenticationProvider? {
+    fun authProvider(
+        passwordEncoder: PasswordEncoder,
+        userDetailsService: UserDetailsService
+    ): DaoAuthenticationProvider? {
         val authProvider = DaoAuthenticationProvider()
         authProvider.setUserDetailsService(userDetailsService)
         authProvider.setPasswordEncoder(passwordEncoder)
         return authProvider
     }
 
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource? {
-        val configuration = CorsConfiguration().apply {
-            allowedOrigins = listOf("http://localhost:8080")
-            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            allowedHeaders = listOf("*")
-        }
-        val source = UrlBasedCorsConfigurationSource().apply {
-            registerCorsConfiguration("/**", configuration)
-        }
-        return source
-    }
+/* @Bean
+ fun corsConfigurationSource(): CorsConfigurationSource {
+     val configuration = CorsConfiguration().apply {
+         allowedOrigins = listOf("http://localhost:8080", "http://localhost:3000")
+         allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+         allowedHeaders = listOf("*")
+     }
+     val source = UrlBasedCorsConfigurationSource().apply {
+         registerCorsConfiguration("/**", configuration)
+     }
+     return source
+ }
+ */
+ */
 
     fun getLoggedInUserEmail(): String {
         val authentication = SecurityContextHolder.getContext().authentication

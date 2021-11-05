2898bd661962e752737641c1c4aa606a0fd25bd9
Matusek Márton <matusekma@gmail.com>
2021-11-05 09:45:25 +0100

    Merge main

--- auth/src/main/kotlin/hu/bme/aut/textanalyzerauth/feature/login/controller/LoginController.kt
+++ auth/src/main/kotlin/hu/bme/aut/textanalyzerauth/feature/login/controller/LoginController.kt
@@ -1,14 +1,18 @@
 package hu.bme.aut.textanalyzerauth.feature.login.controller
 
+import hu.bme.aut.textanalyzerauth.exception.UnauthorizedException
 import hu.bme.aut.textanalyzerauth.feature.login.dto.LoginRequest
 import hu.bme.aut.textanalyzerauth.feature.login.dto.LoginResponse
+import hu.bme.aut.textanalyzerauth.feature.login.dto.UserResponse
 import hu.bme.aut.textanalyzerauth.feature.login.service.LoginService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @RestController
@@ -29,9 +33,17 @@ class LoginController(
     private fun getAccessTokenCookie(token: String) =
             Cookie(accessTokenCookieName, token)
                     .apply {
-                        // TODO uncomment for https - secure = true
+                        path = "/"
+                        maxAge = 36_000 // 10 hours
+                        secure = false // TODO for https - secure = true
                         isHttpOnly = true
                     }
 
+    @GetMapping("/me")
+    fun me(request: HttpServletRequest): UserResponse {
+        val accessToken = request.cookies.firstOrNull { c -> c.name == accessTokenCookieName }
+                ?: throw UnauthorizedException("No access token for /me")
+        return loginService.getCurrentUser(accessToken.value)
+    }
 
 }
